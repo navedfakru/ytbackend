1121d1255f2a2daa82649f04a51b90dcde4ffef0
Naved Ali <naved10055@gmail.com>
2024-07-18 19:07:54 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 // require('dotenv').config({path: './env'})
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-  path: './env'
-})
+  path: "./env",
+});
 
 connectDB()
-
-
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERROR: ", error);
+      throw error;
+    });
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running at port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB connection failed !!! ", err);
+  });
 
 /*
 import express from "express";
